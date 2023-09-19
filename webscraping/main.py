60624485f9225dd0da2569148817f60a2873d5df
fishkatsu <oeykevinwijaya@gmail.com>
2023-09-19 11:02:21 +1000

    Beautiful Soup, able to retrieve Q and A from the website successfully installation text for installation guides

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,5 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.swinburneonline.edu.au/faqs/')
-soup = BeautifulSoup(html_text.text, 'lxml')
+url = 'https://www.swinburneonline.edu.au/faqs/'
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find all the FAQ cards
+    faq_cards = soup.find_all('div', class_='card')
+
+    for index, card in enumerate(faq_cards, start=1):
+        # Extract text content within the card
+        card_text = card.get_text(separator=' ')
+        
+        # Split the text into question and answer
+        parts = card_text.split('A.', 1)  # Split at 'A.' to separate question and answer
+        if len(parts) == 2:
+            question = parts[0].strip().replace('Q.', '').strip()
+            answer = parts[1].strip()
+            
+            # Print the question and answer for each FAQ
+            print(f'FAQ {index}:')
+            print(f'Question: {question}')
+            print(f'Answer: {answer}')
+            print('-' * 50)
+
+else:
+    print('Failed to retrieve the web page. Status code:', response.status_code)

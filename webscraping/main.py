89180b9c780609d4e4b4fd8db5175e1e0458c105
fishkatsu <oeykevinwijaya@gmail.com>
2023-09-19 11:15:45 +1000

    adding main.py function for saving retrieved data to faq_data.csv

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 url = 'https://www.swinburneonline.edu.au/faqs/'
 response = requests.get(url)
@@ -9,6 +10,9 @@ if response.status_code == 200:
     
     # Find all the FAQ cards
     faq_cards = soup.find_all('div', class_='card')
+    
+    # Initialize an empty list to store the extracted data
+    faq_data = []
 
     for index, card in enumerate(faq_cards, start=1):
         # Extract text content within the card
@@ -19,12 +23,25 @@ if response.status_code == 200:
         if len(parts) == 2:
             question = parts[0].strip().replace('Q.', '').strip()
             answer = parts[1].strip()
-            
+
+            # Append the extracted data to the list
+            faq_data.append({"Question": question, "Answer": answer})
+
             # Print the question and answer for each FAQ
             print(f'FAQ {index}:')
             print(f'Question: {question}')
             print(f'Answer: {answer}')
             print('-' * 50)
+
+    # Define the CSV file path
+    csv_file_path = 'faq_data.csv'
+
+    # Save the data to a CSV file
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ["Question", "Answer"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(faq_data)
 
 else:
     print('Failed to retrieve the web page. Status code:', response.status_code)

5100bd047306598e004d9768ae607563a5ecc568
MillieYsm <manarine97@gmail.com>
2023-09-25 18:48:55 +1000

    give recommendations based on similarity score

--- app.py
+++ app.py
@@ -1,23 +1,66 @@
 from flask import Flask, render_template, request
 from webscraping.main import chatbot_function  # Adjust the import path as needed
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+# Load and process FAQ data (move this part from main.py to app.py)
+url = "https://www.swinburneonline.edu.au/faqs/"
+response = requests.get(url)
+faq_data = []
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # Find all the FAQ cards
+    faq_cards = soup.find_all("div", class_="card")
+
+    # Collect FAQ questions and answers
+    for card in faq_cards:
+        card_text = card.get_text(separator=" ")
+        parts = card_text.split("A.", 1)
+        if len(parts) == 2:
+            question = parts[0].strip().replace("Q.", "").strip()
+            answer = parts[1].strip()
+            faq_data.append({"Question": question, "Answer": answer})
+else:
+    print("Failed to retrieve the web page. Status code:", response.status_code)
 
 # Define a route for the homepage
 @app.route('/')
 def home():
-    # Render the homepage template
-    return render_template('index.html', answer=None)
+    # Check if there is an answer to display
+    greeting = "Hello! How may I assist you with Swinburne Online's FAQ today?" 
+    # Render the homepage template and pass the answer to it
+    return render_template('index.html', answer=greeting)
 
 # Define a route to handle user input and display responses
 @app.route('/ask', methods=['POST'])
 def ask():
     # Get the user's question from the form
     user_question = request.form.get('user_question')
-    # Pass the question to the chatbot function
-    response = chatbot_function(user_question)
-    # Render the homepage template with the response
-    return render_template('index.html', answer=response)  # Pass the response to the template
+
+    # Check if user selected a suggestion
+    selected_suggestion = request.form.get('selected_suggestion')
+
+    if selected_suggestion:
+        # User selected a suggestion, find the corresponding answer
+        selected_answer = next(faq["Answer"] for faq in faq_data if faq["Question"] == selected_suggestion)
+        return render_template('index.html', answer=selected_answer, suggestions=None)
+
+    # If no suggestion is selected, pass the question and FAQ data to the chatbot function
+    response, suggestions = chatbot_function(user_question, faq_data)
+
+    if suggestions:
+        # Define the default message if suggestions are available
+        default_message = "Here are some suggestions for you."
+    else:
+        # If no suggestions, set the default message to an empty string
+        default_message = ""
+
+    # Render the homepage template with the default message, response, and suggestions
+    return render_template('index.html', answer=default_message if default_message else response, suggestions=suggestions)
 
 # Run the app
 if __name__ == '__main__':
--- webscraping/main.py
+++ webscraping/main.py
@@ -7,8 +7,8 @@ import csv
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
-# Function to retrieve FAQ data and calculate similarity scores
-def chatbot_function(user_query):
+# Modify the chatbot_function to handle suggestions when scores are close
+def chatbot_function(user_query, faq_data):
     # Retrieve FAQ data from the website
     url = "https://www.swinburneonline.edu.au/faqs/"
     response = requests.get(url)
@@ -31,6 +31,9 @@ def chatbot_function(user_query):
                 answer = parts[1].strip()
                 faq_data.append({"Question": question, "Answer": answer})
 
+        # Define the threshold for a high similarity score
+        threshold = 0.8 
+        
         # Calculate similarity scores for all FAQ questions
         similarity_scores = []
 
@@ -38,7 +41,6 @@ def chatbot_function(user_query):
         for faq in faq_data:
             # Calculate the similarity score between the user query and the FAQ question
             similarity_score = util.pytorch_cos_sim(
-                # Convert the user query and FAQ question to embeddings
                 model.encode(user_query, convert_to_tensor=True),
                 model.encode(faq["Question"], convert_to_tensor=True),
             )
@@ -49,14 +51,6 @@ def chatbot_function(user_query):
                 }
             )
 
-        # Find the FAQ question with the highest similarity score
-        top_question = max(similarity_scores, key=lambda x: x["Similarity Score"])
-        selected_answer = next(
-            faq["Answer"]
-            for faq in faq_data
-            if faq["Question"] == top_question["Question"]
-        )
-
         # Output similarity scores to a CSV file (debugging)
         csv_file_path = "faq_similarity_scores.csv"
         with open(csv_file_path, "w", newline="", encoding="utf-8") as csv_file:
@@ -65,7 +59,33 @@ def chatbot_function(user_query):
             writer.writeheader()
             writer.writerows(similarity_scores)
 
-        return selected_answer
+        # Sort similarity scores
+        sorted_similarity_scores = sorted(similarity_scores, key=lambda x: x["Similarity Score"], reverse=True)
+        top_three_questions = sorted_similarity_scores[:3]
+
+        # Define a flag to track whether an answer has been displayed
+        answer_displayed = False
+
+        # Check if there is at least one high similarity score above the threshold
+        top_score = top_three_questions[0]["Similarity Score"]
+        if top_score >= threshold and not answer_displayed:
+            # Filter questions with scores close to the top score
+            close_scores = [faq for faq in top_three_questions if top_score - faq["Similarity Score"] <= 0.2]
+
+            if len(close_scores) == 1:
+                # If there's only one, return its answer
+                selected_answer = next(faq["Answer"] for faq in faq_data if faq["Question"] == close_scores[0]["Question"])
+                answer_displayed = True
+                return selected_answer, []
+
+        # Check if suggestions should be displayed
+        if not answer_displayed:
+            # If an answer hasn't been displayed, return suggestions
+            suggestions = [faq["Question"] for faq in top_three_questions]
+            return None, suggestions
+        else:
+            # If an answer has been displayed, return an empty suggestion list
+            return None, []
 
     else:
         return "Failed to retrieve the web page. Status code:", response.status_code
